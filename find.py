8ab42c8c1c248a2413db1e5ba4b54e4f3562ebc2
janvipatil585-collab <janvipatil585@gmail.com>
2026-02-08 21:23:38 +0530

    Add files via upload
    finding an element weather it is present or not in a tuple using while loop and else statement here while is a loop and else is a conditional statement used

---
+++ find.py
@@ -0,0 +1,10 @@
+tpl=(1,2,34,56,7,8,90,43,23,45,67,89,12)
+
+i=0
+while i<len(tpl):
+    if tpl[i]==45:
+        print("Element found at index",i)
+        
+    else:
+        print("Finding....")
+    i+=1
